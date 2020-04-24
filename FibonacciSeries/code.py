76b574671707fbdbee44dcee801f5333f0290416
Dushyant Sharma <dushyant.sharma1997@gmail.com>
2020-04-25 03:33:38 +0530

    Update code.py

--- FibonacciSeries/code.py
+++ FibonacciSeries/code.py
@@ -11,7 +11,16 @@ def topdown_fib(n):
             result = topdown_fib(n-1) + topdown_fib(n-2)
             already_fib[n-1] = result
             return result
-
+        
+# Bottom Up Apporach or Tabulation Method
+def bottomup_fib(n):
+    a={}
+    a[0]=0
+    a[1]=1
+    for i in range(2,n+1):
+        a[i]=a[i-1] + a[i-2]
+    return a[n]        
+    
 x = int(input("Enter the number\n"))
 already_fib=[-1]*x
 print("The %dth fibonacci number is "% x, topdown_fib(x))
